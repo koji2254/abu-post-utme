2c29622bb1b839d9de269f1aaeffe7af1dd35440
Koji <66537010+koji2254@users.noreply.github.com>
2024-08-19 22:37:46 +0100

    Fixes Addons 3.2

--- src/assets/Proxy.js
+++ src/assets/Proxy.js
@@ -1,3 +1,3 @@
-export const API_URL_BASE = 'http://127.0.0.1:8000';
-// export const API_URL_BASE = 'https://post-utme.quths.com.ng';
+// export const API_URL_BASE = 'http://127.0.0.1:8000';
+export const API_URL_BASE = 'https://post-utme.quths.com.ng';
 
